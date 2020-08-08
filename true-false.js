e4d1dcbfe4c86bec717a09a49d93c51805b42301
tamimkarim <67522154+tamimkarim@users.noreply.github.com>
2020-08-09 04:11:11 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,14 @@
+//Falsy
+//0
+//""
+//null
+//NaN
+//Truthy
+//'0' ' ' {}
+let name = {};
+if (name == 0 || name) {
+    console.log("condition is true");    
+}
+else{
+    console.log("condition is false");
+}
