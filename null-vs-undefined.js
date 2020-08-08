b43bdc478cc261a3a6705accda82c54df6f55a8b
tamimkarim <67522154+tamimkarim@users.noreply.github.com>
2020-08-09 04:43:23 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+let pakhi;
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+
+
+const b23 = {name: "Tamim Karim", phone : 01822422316};
+console.log(b23.job);
+
+let fun = undefined;
+console.log(fun);
+Undefined is falsy
+
+let ages = [2,5,7];
+console.log(ages[20]);
