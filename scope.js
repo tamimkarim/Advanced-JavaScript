970109604957472f6436726ed04502794bb96198
tamimkarim <67522154+tamimkarim@users.noreply.github.com>
2020-08-10 01:59:59 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,16 @@
+
+let bonus = 20;
+
+function sum(first , second) {
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if (result > 9) {
+        const mood = "happy";
+        console.log(mood);
+    }
+
+    return result;
+}
+const output = sum(3, 7);
+console.log(output);
+console.log(bonus);
